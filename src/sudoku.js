05aaa1b3542b8ff955c0da3df3122b9a63d52ddc
Jake <jparker22@wisc.edu>
2020-03-14 15:51:00 -0500

    pencil in numbers. Lots of bugs + visual problems.

--- src/sudoku.js
+++ src/sudoku.js
@@ -1,8 +1,32 @@
 
 /* jshint -W069, esversion:6 */
 
+// global data for board
 let width = 500.0; let height = 500.0;
 let selected = null;
+let boardVals = [
+    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '0', '0', '0', '0'],
+];
+
+let boardMutable = [
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+    [true, true, true, true, true, true, true, true, true],
+]
 
 window.onload = function() {
     /** @type {HTMLCanvasElement} */
@@ -53,6 +77,11 @@ window.onload = function() {
         // fill in the remaining 3x3 squares
     }, false);
 
+    window.addEventListener("keydown", function(evt) {
+        if (evt.key < '1' || evt.key > '9') return;
+        pencilCurrentCell(evt.key);
+        drawBoard(context);
+    });
     animate(context);
 }
 
@@ -90,6 +119,18 @@ function getMousePos(canvas, evt) {
       y: evt.clientY - rect.top
     };
   }
+
+function pencilCurrentCell(input) {
+    // check if a cell is selected or if selected cell is empty
+    let row = Math.floor(selected[0]/width*10);
+    let col = Math.floor(selected[1]/width*10)+1;
+    console.log(Math.floor(selected[0]/width*10)+1);
+    if (selected === null || boardVals[row][col] !== '0')
+        return;
+    if (!boardMutable[row][col])
+        return;
+    boardVals[row][col] = input;
+}
 
 function drawBoard(/** @type {CanvasRenderingContext2D} */ context) {
     context.save();
@@ -130,5 +171,17 @@ function drawBoard(/** @type {CanvasRenderingContext2D} */ context) {
         context.lineTo(width, height * i / 9);
         context.stroke();
     }
+
+    for (let i=0; i<9; i++) {
+        for (let j=0; j<9; j++) {
+            if (boardVals[i][j] !== '0') {
+                let col = i * width / 9;
+                let row = j * height / 9;
+                context.font = "30px Arial";
+                context.fillText(boardVals[i][j], col, row, width / 9.0, height / 9.0);
+            }
+        }
+    }
+
     context.restore();
 }
